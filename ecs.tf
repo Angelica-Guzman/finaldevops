3babb0d69bb5b7ac194309d09fa7f31f79f9b434
andresctirado <andresctirado@gmail.com>
2024-02-23 16:43:37 -0500

    fix line

--- ecs.tf
+++ ecs.tf
@@ -129,7 +129,7 @@ resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
   role       = aws_iam_role.ecsTaskExecutionRole.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
-98
+
 resource "aws_iam_role" "ecsTaskRole" {
   name               = "ecsTaskRole"
   assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
