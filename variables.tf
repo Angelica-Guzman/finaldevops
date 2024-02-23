44af1ff70d60f47e3a1bc94f35a3aea0a8d7281d
Angelica-Guzman <159931708+Angelica-Guzman@users.noreply.github.com>
2024-02-23 11:33:47 -0500

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "az1" {
+  description = "Availability Zone 1"
+  type        = string
+  default     = "us-east-1a"
+}
+
+variable "az2" {
+  description = "Availability Zone 2"
+  type        = string
+  default     = "us-east-1b"
+}
+
+variable "s3_to_cloudfront" {
+  description = "s3 bucket to Cloudfront"
+  type        = string
+  default     = "s3-cloudfront-sange"
+}
+
+variable "container_port" {
+  description = "container port"
+  type        = number
+  default     = 8080
+}
